de2d836a41a48e0be5fc67f0e68892aceb428d37
luiz.ferreira <luiz.ferreira@PMO4041CGI002.pmo.local>
2019-08-05 14:18:14 -0300

    ~. ~

--- util/corpo.php
+++ util/corpo.php
@@ -1,32 +1,81 @@
-<?php
+<?php 
 
 function cabeca(){
     
     echo '
+<!doctype html>   
+<html>    
    <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="css/bootstrap.min.css" >
-<link rel="stylesheet" href="css/estilo.css" >
-<script type="text/javascript" src="js/feather.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script><script type="text/javascript" src="util/jq/jquery-1.7.1.min.js"></script>
-   
-<script type="text/javascript" src="js/validaCampos.js"></script>
-      <script type="text/javascript" src="js/geraNotificacao.js"></script>
-      <script type="text/javascript" src="js/jquery.form.js"></script>
-      <script type="text/javascript" src="js/jquery.form.js"></script>
-      <script type="text/javascript" src="js/jquery.noty.js"></script>
-      <script type="text/javascript" src="js/jquery.noty.packaged.js"></script>
+        <title>Sistema</title>
+        <script type="text/javascript" src="../vendor/jquery/jquery.js"></script>
+        <script type="text/javascript" src="../vendor/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="../vendor/jquery/jquery.min.map"></script>
+        <script type="text/javascript" src="../vendor/jquery/jquery.slim.js"></script>
+        <script type="text/javascript" src="../vendor/jquery/jquery.slim.min.js"></script>
+        <script type="text/javascript" src="../vendor/jquery/jquery.slim.min.map"></script>
+        <link type="text/css" href="./vendor/bootstrap/css/bootstrap-grid.css" />
+        <link type="text/css" href="./vendor/bootstrap/css/bootstrap-grid.min.css" />
+        <link type="text/css" href="./vendor/bootstrap/css/bootstrap-reboot.css" />
+        <link type="text/css" href="./vendor/bootstrap/css/bootstrap-reboot.min.css" />
+        <link type="text/css" href="./vendor/bootstrap/css/simple-sidebar.css" />
+        <link type="text/css" href="./vendor/bootstrap/css/bootstrap.min.css" />
    </head>
 <body>  
-
 <div class="menu" id="menu"><br>
         <h1><span data-feather="file-text"></span>Sistema de Gestão de Documentos </h1>
     </div>
     <div class="bg-dark barra"></div>
     <br>
+    <div class="menu2 navbar-left bg-dark">
+    <br>
+    <ul class="nav flex-column alinhaUl">
+
+        <li class="nav-item">
+            <span data-feather="home"></span> <a  href="#">Inicio</a>
+        </li>
+        <br>
+        <li class="nav-item">
+            <span data-feather="file-text"></span><a  href="#">Cadastrar Documento</a>
+        </li>
+        <br>
+        <li class="nav-item">
+            <span data-feather="file"></span><a  href="#">Cadastrar Tipo de Documento</a>
+        </li>
+         <br>
+        <li class="nav-item">
+            <span data-feather="user-plus"></span><a href="#">Cadastrar Usuário</a>
+        </li>
+         <br>
+        <li class="nav-item">
+            <span data-feather="lock"></span><a href="#">Cadastrar Permissão</a>
+        </li>
+         <br>
+        <li class="nav-item">
+            <span data-feather="database"></span><a href="#">Listar Documentos</a>
+        </li>
+         <br>
+        <li class="nav-item">
+            <span data-feather="hard-drive"></span><a href="#">Listar Tipo Documento</a>
+        </li>
+         <br>
+        <li class="nav-item">
+            <span data-feather="user-check"></span><a href="#">Listar Usuário</a>
+        </li>
+         <br>
+        <li class="nav-item">
+            <span data-feather="cpu"></span><a href="#">Listar Log</a>
+        </li>
+         <br>
+        <li class="nav-item">
+            <span data-feather="log-out"></span><a href="#">Logout</a>
+        </li>
+         <br>
+       
+    </ul>
+</div>
     
+ <div class="container">
+
 '
     ;
     
@@ -34,26 +83,29 @@ function cabeca(){
 
 function rodape(){
     
-    echo "  
-<script type='text/javascript'>
+    echo "
+   <script type='text/javascript'>
+    $('#menu-toggle').click(function(e) {
+      e.preventDefault();
+      $('#wrapper').toggleClass('toggled');
+    });
+ </script>
+    <script type='text/javascript'>
             feather.replace();
-        </script>        
-<script>
-         var messageUsuario='O campo usuário está vazio!';
-         var messageSenha='O campo senha está vazio!';
-         document.addEventListener('keypress', function(e){
-            if(e.which == 13){
-               validaCamposFormLogin(messageUsuario,messageSenha);
-            }
-         }, false);
-      </script>
+        </script>
+    </div>
+
+
       <nav class='navbar fixed-bottom navbar-light bg-light justify-content-center '>
   <center><p class='alinha'> Sistema de Gestão de Arquivos - Prefeitura Municipal de Olinda º 2019  </p></center>
 </nav>
+ </div>
    </body>
    
 </html>";
+ 
 };
+
 
 function descricaoLogin (){
   
